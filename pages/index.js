33a0cdeed16062364c1ae9196e99a95af9bf87ec
venture-renture <130249362+venture-renture@users.noreply.github.com>
2023-04-11 23:20:41 +0000

    fwfw

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import { Context } from "../context";
+
+import {useRouter } from "next/router";
+
+import axios from "axios";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const{username, setUsername, secret, setSecret,} = useContext(Context)
+
+  const router = useRouter()
+
+  function onSubmit(e) {
+    e.preventDefault()
+
+    if (username.length ===0 || secret.length === 0) return;
+
+    axios.put(
+      'https://api.chatengine.io/users/',
+      {username, secret},
+      {headers: {"Private-key": '3b50993d-410e-480f-b9d4-8da311c2f43f'}}
+    )
+    .then(r => router.push('/chats'));
+  }
+  return (
+  <div className="background">
+    <div className='auth-container'>
+      <form className='auth-form' onSubmit={e => onSubmit(e)}>
+      <div className='auth-title'>Stream Chat</div>
+
+      <div className='input-container'>
+        <input 
+          placeholder='Email'
+          className='text-input'
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+
+      <div className='input-container'>
+        <input 
+          type='password'
+          placeholder='Password'
+          className='text-input'
+          onChange={e => setSecret(e.target.value)}
+        />
+      </div>
+
+      <button
+      type='submit'
+      className='submit-button'
+      >
+        Login/Sign Up
+      </button>
+      </form>
+    </div>
+  </div>
+  )
 }
